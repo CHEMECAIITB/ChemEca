c802f2fcb3e5b434341e481baa1713e78dafba20
lokesh <racing.lokesh2024@gmail.com>
2025-08-01 03:05:35 +0530

    add dlj

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,71 +1,61 @@
 import React, { useEffect } from 'react';
-import Header from '../components/Header';
+import CustomCursor from '../components/CustomCursor'
+import Header from '../components/Header'
+import './Contact.css'
 import Footer from '../components/Footer';
-import './Contact.css';
 
 const Contact = () => {
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
-
   return (
     <>
-      <Header />
-      <div className="contact-us mt-28">
-        {/* Title Section */}
-        <div className="header">
-          <h1>Get in Touch with Us</h1>
-          <h2>CONTACT US</h2>
-          <p>
-            We are available to help you with inquiries related to competitions, projects, or collaborations.
-            Feel free to reach out!
-          </p>
+    <Header/>
+    <div className="contact-us mt-28">
+      <div className="header">
+        <h1>Get in Touch with Us</h1>
+        <h2>CONTACT US</h2>
+        <p>
+          
+        </p>
+      </div>
+      <div className="contact-info">
+        <div className="info">
+          <div className="icon phone-icon" />
+          <p>Phone No.</p>
+          <ul>
+            <li>Yogesh Dangi : 7067503163</li>
+            <li>Saransh vottery : 75090 40303</li>
+          </ul>
         </div>
-
-        {/* Info Grid */}
-        <div className="contact-info">
-          <div className="info">
-            <div className="icon phone-icon" />
-            <p>Phone No.</p>
-            <ul>
-              <li>Harsh: 9409475801</li>
-              <li>Gaurav: 8669723750</li>
-              <li>Khush: 9328441633</li>
-            </ul>
-          </div>
-
-          <div className="info">
-            <div className="icon email-icon" />
-            <p>E-mail</p>
-            <ul>
-              <li>chemeca.iitb@gmail.com</li>
-            </ul>
-          </div>
-
-          <div className="info">
-            <div className="icon address-icon" />
-            <p>Address</p>
-            <ul>
-              <li>
-                ChemEca Lab, F3 Shed, Chemical Engineering Department,
-                IIT Bombay, Powai, Mumbai, Maharashtra, India. PIN - 400076
-              </li>
-            </ul>
-          </div>
-
-          <div className="info">
-            <div className="icon clock-icon" />
-            <p>Peak Hours</p>
-            <ul>
-              <li>Monday - Sunday</li>
-              <li>10:00 AM to 12:00 PM</li>
-            </ul>
-          </div>
+        <div className="info">
+          <div className="icon email-icon" />
+          <p>E-mail</p>
+          <ul>
+            <li>chemeca.iitb@gmail.com
+            </li>
+          </ul>
+        </div>
+        <div className="info">
+          <div className="icon address-icon" />
+          <p>Address</p>
+          <ul>
+            <li>ChemEca Lab, F3 shed, Chemcial Engineering, IIT Bombay, Powai, Mumbai, Maharashtra, India. PIN - 400076</li>
+          </ul>
+        </div>
+        <div className="info">
+          <div className="icon clock-icon" />
+          <p>Peak Hours</p>
+          <ul>
+            <li>Monday - Sunday (10:00 PM to 12:00 PM)</li>
+          </ul>
         </div>
       </div>
-      <Footer />
-    </>
-  );
-};
+    </div>
 
-export default Contact;
+    <Footer/>
+    </>
+  )
+}
+
+export default Contact
--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -21,18 +21,18 @@ import img14 from '../assets/profiles/img14.png';
 import img17 from '../assets/profiles/img17.jpeg';
 import img18 from '../assets/profiles/img18.jpg';
 import img19 from '../assets/profiles/img19.png';
-// import img20 from '../assets/profiles/img20.jpeg';
-// import img21 from '../assets/profiles/img21.jpeg';
-// import img22 from '../assets/profiles/img22.jpeg';
-// import img23 from '../assets/profiles/img23.jpeg';
-// import img24 from '../assets/profiles/img24.jpeg';
-// import img25 from '../assets/profiles/img25.jpeg';
+import img20 from '../assets/profiles/img20.jpg';
+import img21 from '../assets/profiles/img21.jpg';
+import img22 from '../assets/profiles/img22.jpg';
+import img23 from '../assets/profiles/img23.jpg';
+import img24 from '../assets/profiles/img24.jpg';
+import img25 from '../assets/profiles/img25.jpeg';
 // import img26 from '../assets/profiles/img26.jpeg';
-// import img27 from '../assets/profiles/img27.jpeg';
+import img27 from '../assets/profiles/img27.jpg';   // vipul amrutkar
 // import img28 from '../assets/profiles/img28.jpeg';
-// import img29 from '../assets/profiles/img29.jpeg';
-// import img30 from '../assets/profiles/img30.jpeg';
-// import img31 from '../assets/profiles/img31.jpeg';
+import img29 from '../assets/profiles/img29.jpg';
+import img30 from '../assets/profiles/img30.jpg';
+import img31 from '../assets/profiles/img31.png';
 // import img32 from '../assets/profiles/img32.jpeg';
 // import img33 from '../assets/profiles/img33.jpeg';
 // import img34 from '../assets/profiles/img34.jpeg';
@@ -40,6 +40,7 @@ import img19 from '../assets/profiles/img19.png';
 // import img36 from '../assets/profiles/img36.jpeg';
 import Footer from '../components/Footer';
 import { useEffect } from "react";
+import { image } from 'framer-motion/client';
 const Team = () => {
   useEffect(() => {
     window.scrollTo(0, 0);
@@ -52,47 +53,55 @@ const Team = () => {
     
     { "name": "Sandip Roy", "email": "sr@iitb.ac.in", "position": "Faculty Advisor", "division": "Faculty Advisor", "image": img2},
 
-    // PhD Advisor
-    { "name": "Vipul Amrutkar", "email": "23d0139@iitb.ac.in", "position": "PhD Advisor", "division": "PhD Advisor" },
+    // Advisors
+    { "name": "Vipul Amrutkar", "email": "23d0139@iitb.ac.in", "position": "PhD Advisor", "division": "Advisors", "image": img27 },
+    { "name": "Maithri Suresh", "email": "maithri@iitb.ac.in", "position": "UG Dual Degree", "division": "Advisors", "image": img22 },
   
     // Team Lead
-    { "name": "Bhavana", "email": "210020078@iitb.ac.in", "position": "Team Lead", "division": "Team Lead", "image": img8 },
-    { "name": "Yogesh Dangi", "email": "yogesh.dangi@iitb.ac.in", "position": "Team Lead", "division": "Team Lead", "image": img9 },
+    { "name": "Saransh Vottery ", "email": "22B0348@iitb.ac.in", "position": "Team Lead", "division": "Team Leads", "image": img10 },
+    { "name": "Yogesh Dangi", "email": "yogesh.dangi@iitb.ac.in", "position": "Team Lead", "division": "Team Leads", "image": img9 },
   
-    // Research Subsystem
-    { "name": "Saransh Vottery", "email": "22B0348@iitb.ac.in", "position": "Research Lead", "division": "Research Subsystem", "image": img10 },
-    { "name": "Arush Gaur", "email": "210020022@iitb.ac.in", "position": "Senior Advisor", "division": "Research Subsystem", "image": img11 },
-    { "name": "Pranav P. Kawade", "email": "23B0372@iitb.ac.in", "position": "Research Associate", "division": "Research Subsystem" },
-    { "name": "Sai Tambaku", "email": "23B0351@iitb.ac.in", "position": "Research Associate", "division": "Research Subsystem" },
-    { "name": "Shivam Gupta", "email": "23b0309@iitb.ac.in", "position": "Research Associate", "division": "Research Subsystem" ,"image": img14},
-    { "name": "Darppan MV", "email": "22b0436@iitb.ac.in", "position": "Research Associate", "division": "Research Subsystem" },
-    { "name": "Eshika Mishra", "email": "23B0371@iitb.ac.in", "position": "Research Associate", "division": "Research Subsystem" },
+    // Research & Development
+    { "name": "Jayant Deshmukh", "email": "23B0431@iitb.ac.in", "position": "Subsystem Lead", "division": "Research & Development", "image": img23 },
+    // { "name": "Arush Gaur", "email": "210020022@iitb.ac.in", "position": "Senior Advisor", "division": "Research Subsystem", "image": img11 },
+    // { "name": "Pranav P. Kawade", "email": "23B0372@iitb.ac.in", "position": "Research Associate", "division": "Research Subsystem" },
+    // { "name": "Sai Tambaku", "email": "23B0351@iitb.ac.in", "position": "Research Associate", "division": "Research Subsystem" },
+    { "name": "Shivam Gupta", "email": "23b0309@iitb.ac.in", "position": "Subsystem Engineer", "division": "Research & Development" ,"image": img31},
+    // { "name": "Darppan MV", "email": "22b0436@iitb.ac.in", "position": "Research Associate", "division": "Research Subsystem" },
+    // { "name": "Eshika Mishra", "email": "23B0371@iitb.ac.in", "position": "Research Associate", "division": "Research Subsystem" },
+   
+    // Process Simulation
+    // { "name": "Gayatri Moghe", "email": "22b2451@iitb.ac.in", "position": "Process and Manufacturing Lead", "division": "Process and Manufacturing Subsystem", "image": img17 },
+    // { "name": "Shruti", "email": "210020132@iitb.ac.in", "position": "Senior Advisor", "division": "Process and Manufacturing Subsystem", "image": img18 },
+    // { "name": "Manas Agarwal", "email": "210020074@iitb.ac.in", "position": "Senior Advisor", "division": "Process and Manufacturing Subsystem", "image": img19 },
+    // { "name": "Mahesh Naik", "email": "23b0350@iitb.ac.in", "position": "Process and Manufacturing Engineer", "division": "Process and Manufacturing Subsystem" },
+    { "name": "OJAS PATHAK", "email": "23b0428@iitb.ac.in", "position": "Subsystem Lead", "division": "Process Simulation" , "image": img29 },
+    { "name": "A Naveen", "email": "naveeen1125@gmail.com", "position": "Subsystem Engineer", "division": "Process Simulation", image: img20 },
+
+    { "name": "Arpit Vikram Prajapati ", "email": "23B0371@iitb.ac.in", "position": "Subsystem Engineer", "division": "Process Simulation" , "image": img30},
     
-    // Process and Manufacturing Subsystem
-    { "name": "Gayatri Moghe", "email": "22b2451@iitb.ac.in", "position": "Process and Manufacturing Lead", "division": "Process and Manufacturing Subsystem", "image": img17 },
-    { "name": "Shruti", "email": "210020132@iitb.ac.in", "position": "Senior Advisor", "division": "Process and Manufacturing Subsystem", "image": img18 },
-    { "name": "Manas Agarwal", "email": "210020074@iitb.ac.in", "position": "Senior Advisor", "division": "Process and Manufacturing Subsystem", "image": img19 },
-    { "name": "Mahesh Naik", "email": "23b0350@iitb.ac.in", "position": "Process and Manufacturing Engineer", "division": "Process and Manufacturing Subsystem" },
-    { "name": "OJAS PATHAK", "email": "23b0428@iitb.ac.in", "position": "Process and Manufacturing Engineer", "division": "Process and Manufacturing Subsystem" },
-    { "name": "A Naveen", "email": "naveeen1125@gmail.com", "position": "Process and Manufacturing Engineer", "division": "Process and Manufacturing Subsystem" },
-    { "name": "Sivakrishna Sasi", "email": "23B2237@iitb.ac.in", "position": "Process and Manufacturing Engineer", "division": "Process and Manufacturing Subsystem" },
-    { "name": "Arth Trivedy", "email": "23b0380@iitb.ac.in", "position": "Process and Manufacturing Engineer", "division": "Process and Manufacturing Subsystem" },
+   
+    // { "name": "Sivakrishna Sasi", "email": "23B2237@iitb.ac.in", "position": "Process and Manufacturing Engineer", "division": "Process and Manufacturing Subsystem" },
+    // { "name": "Arth Trivedy", "email": "23b0380@iitb.ac.in", "position": "Process and Manufacturing Engineer", "division": "Process and Manufacturing Subsystem" },
     
-    // Control and Automation Subsystem
-    { "name": "Shivani", "email": "22b0349@iitb.ac.in", "position": "Control and Automation Lead", "division": "Control and Automation Subsystem" },
-    { "name": "Maithri Suresh", "email": "210020072@iitb.ac.in", "position": "Senior Advisor", "division": "Control and Automation Subsystem" },
-    { "name": "Mridul Choudhary", "email": "23b3933@iitb.ac.in", "position": "Control and Automation Engineer", "division": "Control and Automation Subsystem" },
-    { "name": "Renudhruva S", "email": "renudhruvas@gmail.com", "position": "Control and Automation Engineer", "division": "Control and Automation Subsystem" },
-    { "name": "Mohith H", "email": "iammohith2004@gmail.com", "position": "Control and Automation Engineer", "division": "Control and Automation Subsystem" },
-    { "name": "Mohammad Hasaan Naurez", "email": "hasaannaurez777@gmail.com", "position": "Control and Automation Engineer", "division": "Control and Automation Subsystem" },
-    { "name": "Param Aghera", "email": "23b0447@iitb.ac.in", "position": "Control and Automation Engineer", "division": "Control and Automation Subsystem" },
+    // Instrumentation
+    // { "name": "Shivani", "email": "22b0349@iitb.ac.in", "position": "Control and Automation Lead", "division": "Control and Automation Subsystem" },
+    // { "name": "Maithri Suresh", "email": "210020072@iitb.ac.in", "position": "Senior Advisor", "division": "Control and Automation Subsystem" },
+    // { "name": "Mridul Choudhary", "email": "23b3933@iitb.ac.in", "position": "Control and Automation Engineer", "division": "Control and Automation Subsystem" },
+    // { "name": "Renudhruva S", "email": "renudhruvas@gmail.com", "position": "Control and Automation Engineer", "division": "Control and Automation Subsystem" },
+    // { "name": "Mohith H", "email": "iammohith2004@gmail.com", "position": "Control and Automation Engineer", "division": "Control and Automation Subsystem" },
+    // { "name": "Mohammad Hasaan Naurez", "email": "hasaannaurez777@gmail.com", "position": "Control and Automation Engineer", "division": "Control and Automation Subsystem" },
+    { "name": "Param Aghera", "email": "23b0447@iitb.ac.in", "position": "Subsystem Lead", "division": "Instrumentation" , image: img24 },
+    { "name": "Om Anghan", "email": "23b0338@iitb.ac.in", "position": "Subsystem Engineer", "division": "Instrumentation" , image: img21 },
     
     // Business Subsystem
-    { "name": "Yogesh Dangi", "email": "yogesh.dangi@iitb.ac.in", "position": "Business Lead", "division": "Business Subsystem" },
-    { "name": "Riddhi Agarwal", "email": "riddhi2009@live.com", "position": "Senior Advisor", "division": "Business Subsystem" },
-    { "name": "Vassu Sharma", "email": "23b0409@iitb.ac.in", "position": "Business Associate", "division": "Business Subsystem" },
-    { "name": "Paresh Kumar Sabar", "email": "23b0328@iitb.ac.in", "position": "Business Associate", "division": "Business Subsystem" },
-    { "name": "Ishaan Srivastava", "email": "ishaan0719@gmail.com", "position": "Business Associate", "division": "Business Subsystem" }
+    // { "name": "Yogesh Dangi", "email": "yogesh.dangi@iitb.ac.in", "position": "Business Lead", "division": "Business Subsystem" },
+    // { "name": "Riddhi Agarwal", "email": "riddhi2009@live.com", "position": "Senior Advisor", "division": "Business Subsystem" },
+    // { "name": "Vassu Sharma", "email": "23b0409@iitb.ac.in", "position": "Business Associate", "division": "Business Subsystem" },
+    // { "name": "Paresh Kumar Sabar", "email": "23b0328@iitb.ac.in", "position": "Business Associate", "division": "Business Subsystem" },
+    // { "name": "Ishaan Srivastava", "email": "ishaan0719@gmail.com", "position": "Business Associate", "division": "Business Subsystem" }
+    { "name": "Renudhruva S", "email": "23b0445@iitb.ac.in", "position": "Subsystem Lead", "division": "Business" , image: img25 },
+
   ];
   
   
